644f01e07be0af0261de41c6c07b9da0032236da
Arceden <github@arnoldbuining.me>
2023-01-07 22:46:29 +0100

    SN-2 Change GetAlbumPhotos call in repository to target the correct album

--- SynologyNet/Helpers/RestRequestHelper.cs
+++ SynologyNet/Helpers/RestRequestHelper.cs
@@ -11,9 +11,15 @@ namespace SynologyNet.Helpers
         /// <param name="name">Parameter name</param>
         /// <param name="value">Parameter value</param>
         /// <returns>RestRequest</returns>
-        public static RestRequest AddParameterIfNotNull(this RestRequest request, string name, string value)
+        public static RestRequest AddParameterIfNotNull(this RestRequest request, string name, string? value)
         {
             return value != null ? request.AddParameter(name, value) : request;
+        }
+
+        /// <inheritdoc cref="AddParameterIfNotNull(RestRequest, string, string?)"/>
+        public static RestRequest AddParameterIfNotNull(this RestRequest request, string name, int? value)
+        {
+            return value != null ? request.AddParameter(name, (int) value) : request;
         }
     }
 }
