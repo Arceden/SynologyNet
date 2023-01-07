d0e6ca1bd5635786e196a1140acccfe07411ec0c
Arceden <github@arnoldbuining.me>
2023-01-07 22:49:24 +0100

    Remove nullable string annotation because we are not using the nullable context yet

--- SynologyNet/Helpers/RestRequestHelper.cs
+++ SynologyNet/Helpers/RestRequestHelper.cs
@@ -11,7 +11,7 @@ namespace SynologyNet.Helpers
         /// <param name="name">Parameter name</param>
         /// <param name="value">Parameter value</param>
         /// <returns>RestRequest</returns>
-        public static RestRequest AddParameterIfNotNull(this RestRequest request, string name, string? value)
+        public static RestRequest AddParameterIfNotNull(this RestRequest request, string name, string value)
         {
             return value != null ? request.AddParameter(name, value) : request;
         }
