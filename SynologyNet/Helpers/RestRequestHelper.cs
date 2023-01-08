e77f693407d278e3f7b9bd7ec924418c12359ce3
Arceden <github@arnoldbuining.me>
2023-01-08 17:04:57 +0100

    Remove unused method

--- SynologyNet/Helpers/RestRequestHelper.cs
+++ SynologyNet/Helpers/RestRequestHelper.cs
@@ -1,9 +1,8 @@
 ﻿using RestSharp;
-using System;
 
 namespace SynologyNet.Helpers
 {
-    public static class RestRequestHelper
+    static class RestRequestHelper
     {
         /// <summary>
         /// Add request parameter if value is not null
@@ -13,19 +12,6 @@ namespace SynologyNet.Helpers
         /// <param name="value">Parameter value</param>
         /// <returns>RestRequest</returns>
         public static RestRequest AddParameterIfNotNull(this RestRequest request, string name, string? value)
-        {
-            return value != null ? request.AddParameter(name, value) : request;
-        }
-
-        [Obsolete("Dit werkt niet echt goed ivm enum values")]
-        public static RestRequest AddDynamicParameter(RestRequest request, string name, dynamic value)
-            => Type.GetTypeCode(value.GetType()) switch
-            {
-                TypeCode.Int16 or TypeCode.Int32 or TypeCode.Int64 => request.AddParameter(name, (int) value),
-                TypeCode.String => request.AddParameter(name, (string) value),
-                //TypeCode.Enum => request.AddParameter(name, ((Enum) value).GetValue()),
-                _ => throw new NotImplementedException($"No support for filter with type {value.GetType()}.")
-            };
-
+            => value != null ? request.AddParameter(name, value) : request;
     }
 }
