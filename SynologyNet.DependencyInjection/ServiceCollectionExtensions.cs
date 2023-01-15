1e59c2486fbf337ae3531c77f8d7680e3ef74eb5
Arceden <arnoldbuining@hotmail.com>
2023-01-15 22:53:55 +0100

    Add new project for dependency injection

---
+++ SynologyNet.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace SynologyNet.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddSynology(this IServiceCollection services)
+    {
+        // todo add services
+    }
+
+    public static void AddSynology(this IServiceCollection services, SynologyOptions options)
+    {
+        // todo register login
+
+        AddSynology(services);
+    }
+}
---
+++ SynologyNet.DependencyInjection/SynologyOptions.cs
@@ -0,0 +1,24 @@
+﻿namespace SynologyNet.DependencyInjection;
+
+public record SynologyOptions
+{
+    /// <summary>
+    /// Synology host ip address or hostname
+    /// </summary>
+    public string Host { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Synology host port number
+    /// </summary>
+    public int Port { get; set; } = 5000;
+
+    /// <summary>
+    /// Synology user to log in to
+    /// </summary>
+    public string Username { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Synology user password
+    /// </summary>
+    public string Password { get; set; } = string.Empty;
+}
